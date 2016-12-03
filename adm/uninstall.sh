b400d4f04f7dd0803e81466ac9d049b32c7ed5ff
lixin <sadoneli@gmail.com>
2016-12-03 16:18:11 +0800

    first commit

---
+++ adm/uninstall.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+sh /koolshare/adm/adm.sh stop
+rm -rf /koolshare/adm
+rm -rf /koolshare/init.d/S60adm.sh
+rm -rf /koolshare/scripts/adm_*.sh
+rm -rf /koolshare/webs/Module_adm.asp
+
---
+++ config.json.js
@@ -0,0 +1,9 @@
+{
+    "build_date": "2016-05-14_21:11:08", 
+    "description": "去广告，看疗效~", 
+    "home_url": "Module_adm.asp", 
+    "md5": "a6a6dc70692ab70971365fd68080e0de", 
+    "module": "adm", 
+    "title": "阿呆猫", 
+    "version": "1.5"
+}
